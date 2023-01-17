4738b059b42afd4a983c4959f3accb3b2f174a61
LucaBonifazio <luca.bonifazio13@gmail.com>
2023-01-17 10:55:17 +0100

    responsive card

--- resources/views/guest/main.blade.php
+++ resources/views/guest/main.blade.php
@@ -1,11 +1,11 @@
 @extends('guest.layouts.base')
 
 @section('content')
-<div class="row row-cols-4 g-2 p-5">
+<div class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-2 p-5">
     @foreach ($movies as $movie)
     <div class="col">
             <a href="/movie">
-                <div class="card" style="width: 18rem; height: 12rem;">
+                <div class="card h-100">
                     <div class="card-body">
                     <h5 class="card-title">Title: {{$movie['title']}}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Original title: {{$movie['original_title']}}</h6>
--- resources/views/guest/movie.blade.php
+++ resources/views/guest/movie.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="row p-5">
     @foreach ($movies as $movie)
-    <div class="col">
+    <div class="col-3">
             <a href="/movie">
-                <div class="card" style="width: 18rem;">
+                <div class="card h-100">
                     <div class="card-body">
                     <h5 class="card-title">Title: {{$movie['title']}}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Original title: {{$movie['original_title']}}</h6>
